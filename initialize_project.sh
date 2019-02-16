8cdee9ccaa9bad622b889274a8e16a6034d5adf9
zzusoftboy <zzu_softboy@163.com>
2019-02-16 23:41:08 +0800

    check link exist

--- initialize_project.sh
+++ initialize_project.sh
@@ -1,4 +1,4 @@
 composer config repo.packagist composer https://packagist.phpcomposer.com
 composer require apigen/apigen --dev
 curdir=`pwd`
-ln -s ${curdir}/apigen-theme-polarphp/ vendor/apigen/theme-polarphp
+[ ! -s vendor/apigen/theme-polarphp ] && ln -s ${curdir}/apigen-theme-polarphp vendor/apigen/theme-polarphp
