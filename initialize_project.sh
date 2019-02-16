bd9da81fd1e70fe273dd329a11bb62f4115f6f0f
zzusoftboy <zzu_softboy@163.com>
2019-02-16 23:29:12 +0800

    fix path error

--- initialize_project.sh
+++ initialize_project.sh
@@ -1,3 +1,4 @@
 composer config repo.packagist composer https://packagist.phpcomposer.com
 composer require apigen/apigen --dev
-ln -s ./apigen-theme-polarphp/ vendor/apigen/theme-polarphp
+curdir=`pwd`
+ln -s ${curdir}/apigen-theme-polarphp/ vendor/apigen/theme-polarphp
