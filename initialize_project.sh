dc7e1904b30a737540a244057568d7b7d912fbc1
zzusoftboy <zzu_softboy@163.com>
2019-02-16 18:37:49 +0800

    add theme submodule

--- initialize_project.sh
+++ initialize_project.sh
@@ -1,3 +1,2 @@
 composer config repo.packagist composer https://packagist.phpcomposer.com
 composer require apigen/apigen --dev
-git submodule add -f polarphp:polarphp/apigen-theme-polarphp.git vendor/apigen/theme-polarphp
